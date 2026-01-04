e629ff58129691e0d57757ffb37fb9c4be0d04d1
DawrAli33285 <lemightyeagle@gmail.com>
2026-01-04 15:21:53 +0500

    new

--- src/baseurl.js
+++ src/baseurl.js
@@ -1,3 +1,3 @@
-// export const BASE_URL='http://13.59.218.198:5000'
+export const BASE_URL='http://3.22.105.106:5000'
 // export const BASE_URL='http://localhost:5000'
-export const BASE_URL='https://wallmartscrappernewbackend.vercel.app'
+// export const BASE_URL='https://wallmartscrappernewbackend.vercel.app'
